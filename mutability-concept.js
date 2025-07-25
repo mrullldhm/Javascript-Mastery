051a4ee2009fd4a0960bbd9a4a2498699faf89b6
mrullldhm <mrullldhm94@gmail.com>
2025-07-26 01:11:24 +0800

    mutability concept

---
+++ mutability-concept.js
@@ -0,0 +1,10 @@
+// Mutability is about whether a value can be changed (mutated) after it has been created
+
+// Mutable - value can be changed
+// Non-Primitive can be changed
+// Object, Array, Map, Set
+
+// Immutable - value cannot be changed
+// Primitive types cannot be changed directly
+// Number, String, Boolean, Undefined, Null, Symbol, BigInt
+
