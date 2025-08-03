b6f0af206ae747c6b1d88ee8b34fd42cb1b77c05
mrullldhm <mrullldhm94@gmail.com>
2025-08-03 17:16:10 +0800

    update content

---
+++ later/2.4-asynchronous-programming.js
@@ -0,0 +1,8 @@
+// Asynchronous programming lets the code run without waiting for long task
+// Long task like API calls, file reads or timers to finish
+
+// Common Async Pattern
+// 1. Callbacks - a function passed into another function to be callerd later
+function getData(callback) {
+    
+}
